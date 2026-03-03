5d080a534af7402283ed287b909de848e0ab0acd
Sergei Tsarik <serge.tsarik@gmail.com>
2026-03-03 08:24:52 +0100

    add explicit wait

--- src/test/java/de/dhl/HomeTest.java
+++ src/test/java/de/dhl/HomeTest.java
@@ -8,25 +8,32 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 
 public class HomeTest {
     private AndroidDriver driver;
 
     @BeforeEach
     public void setUp() throws MalformedURLException {
-        // Настройка параметров (Capabilities) для подключения к устройству
         String appPath = new File("src/test/resources/login-demo-app.apk").getAbsolutePath();
+
         UiAutomator2Options options = new UiAutomator2Options()
                 .setPlatformName("Android")
                 .setDeviceName("emulator-5554")
                 .setApp(appPath)
                 .setAutomationName("UiAutomator2");
 
-        // Инициализация драйвера и подключение к Appium Server
+        // Таймауты для CI
+        options.setCapability("appium:uiautomator2ServerInstallTimeout", 90000);
+        options.setCapability("appium:uiautomator2ServerLaunchTimeout", 90000);
+        options.setCapability("appium:adbExecTimeout", 60000);
+
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), options);
     }
 
@@ -34,18 +41,19 @@ public class HomeTest {
     public void testScreenOpened() {
         String titleMyAppLocator = "//android.view.View[@content-desc=\"Моё приложение\"]";
 
-        //Thread.sleep(4000);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        WebElement titleMyApp = wait.until(
+                ExpectedConditions.presenceOfElementLocated(By.xpath(titleMyAppLocator))
+        );
 
-        WebElement titleMyApp = driver.findElement(By.xpath(titleMyAppLocator));
         String actualContentDescForTitleMyApp = titleMyApp.getAttribute("content-desc");
-
         Assertions.assertEquals("Моё приложение", actualContentDescForTitleMyApp);
     }
 
     @AfterEach
     public void tearDown() {
         if (driver != null) {
-            driver.quit(); // Завершение сессии после каждого теста
+            driver.quit();
         }
     }
 }
